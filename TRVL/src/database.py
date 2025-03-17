ce0c60138c1041fba07bd2f91acb46b6d744a3dc
Dayane <dayanealvescox@gmail.com>
2025-03-17 17:01:46 -0300

    Aula 17/03 Editar nome do usuario, editar viagem, editar senha.

--- TRVL/src/database.py
+++ TRVL/src/database.py
@@ -85,29 +85,71 @@ def mostrar_id_viagens (id_email):
     finally:
         conexao.close()
         
-def apagar_usuario (id_usuario):
+def apagar_usuario (email):
     conexao = conectar_banco()
     cursor = conexao.cursor()
     
     try:
-        cursor.execute('DELETE FROM usuarios WHERE email = ?', (id_usuario,))
+        cursor.execute('DELETE FROM usuarios WHERE email = ?', (email,))
+        cursor.execute('DELETE FROM projetos_de_viagem WHERE id_usuario = ?', (email,))
         conexao.commit()
         return True
     except sqlite3.IntegrityError:
         return False
     finally:
         conexao.close()
+        
+def mudar_nome_usuario( novonome, email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    
+    try:
+        cursor.execute("UPDATE usuarios SET nome = ? WHERE email = ?", (novonome, email))
+        conexao.commit()
+        return True
+    except sqlite3.IntegrityError:
+        return False
+    finally:
+        conexao.close()
+        
+def mudar_senha(novasenha, email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    
+    try:
+        cursor.execute("UPDATE usuarios SET senha = ? WHERE email = ?", (novasenha, email))
+        conexao.commit()
+        return True
+    except sqlite3.IntegrityError:
+        return False
+    finally:
+        conexao.close()
+        
+def editar_viagem(destino, data_prevista, status, gastos, dinheiro_guardado, id_usuario):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    
+    try:
+        cursor.execute("UPDATE projetos_de_viagem SET destino = ?, data_prevista = ?, status = ?, gastos = ?, dinheiro_guardado = ? WHERE id_usuario = ?", (destino, data_prevista, status, gastos, dinheiro_guardado,id_usuario))
+        conexao.commit()
+        return True
+    except sqlite3.IntegrityError:
+        return False
+    finally:
+        conexao.close()
+    
 
 if __name__ == '__main__': 
     conexao = conectar_banco()
-    #criar_tabelas()
+    criar_tabelas()
     id_viagens = mostrar_id_viagens('dayanealvescox@email.com')
     print(id_viagens)
 
-    apagar_viagem("1")
-    apagar_usuario('dayanealvescox@email.com')
-
-    
+    #apagar_viagem("1")
+    #apagar_usuario('dayanealvescox@email.com')
+    mudar_nome_usuario('Renata','renata@email.com' )
+    mudar_senha('12345', 'renata@email.com')
+    editar_viagem('Paris', '25/05/2025','em andamento', '20000', '10000','renata@email.com' )
 
            
         
